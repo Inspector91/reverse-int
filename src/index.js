7df325f420ed8dd868f168b12e8c43d0bc3197b6
Inspector91 <bogdanovsa91@gmail.com>
2022-12-12 21:29:17 +0700

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+if (n < 0) {
+    n = n * -1;
+}
+let first = n % 10;
+let third = Math.trunc(n / 100);
+let second = Math.trunc((n - Math.trunc(n  / 100) * 100) / 10);
+result = Number(`${first}${second}${third}`);
+return result;
 }
